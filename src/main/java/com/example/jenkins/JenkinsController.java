858b896b263f4f8f86de8607f441e24ff968c2b9
rahaman9181 <rahaman9181gmail.com>
2022-03-21 22:33:59 +0530

    first commit

---
+++ src/main/java/com/example/jenkins/JenkinsController.java
@@ -0,0 +1,16 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+	
+	@GetMapping("/get")
+	public String getScope() {
+		return "Hello World";
+		
+	}
+}
